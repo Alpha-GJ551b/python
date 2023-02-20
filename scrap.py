229575c0a2e1efe2fb60015f46c5e041d0c27756
Kevin Romagne <kevin.69.personnel@gmail.com>
2023-02-20 20:02:00 +0100

    test vérification cellules voisines

--- scrap.py
+++ scrap.py
@@ -1,7 +1,8 @@
 import random
 
 #initialisation des variables
-colonnes, lignes = 10, 10
+colonnes, lignes = 2, 2
+nbCell = 0
 
 #génère un tableau de 10 * 10
 tableau = [[random.randint(0,1) for i in range(colonnes)] for j in range(lignes)]
@@ -9,15 +10,22 @@ tableau = [[random.randint(0,1) for i in range(colonnes)] for j in range(lignes)
 #afficher le tableau
 for i in range(colonnes):
     for j in range(lignes):
-        if (tableau[i][j]== 0):
+        if (tableau[i][j] == 0):
             print(' ',end='')
         else :
             print('1',end='')    
     print()
-    
+
 #vérifie les voisins d'une cellule 
-
-
-#while true :
-    
-
+for x in range(colonnes):
+    for y in range(lignes):
+        for i in range(-1,2,1):
+            for j in range(-1,2,1):
+                if (( x + i >= 0) and ( x + i < lignes) and (y + j >= 0) and ( y + j < colonnes)):
+                    if (tableau[x + i][y + j] == 1 ):
+                        if(tableau[x][y] == 1):
+                            pass
+                        else:
+                            nbCell += 1
+        print(nbCell)
+        nbCell = 0            
