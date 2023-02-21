c829dd8fb5d0c6321de585704cd873e6a9ec8bb6
Kevin Romagne <kevin.69.personnel@gmail.com>
2023-02-21 19:20:25 +0100

    ajout fichier csv + récupération des prix

--- scrap.py
+++ scrap.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://www.maier.fr"
 uri = "/occasions"
 page = requests.get(url + uri)
+tab = []
 
 # Voir le code html source
 print(page.ok)
@@ -17,9 +19,16 @@ if page.ok:
         a = li.find("a")
         #print(a)
         print(a["href"])
+        page = requests.get(a["href"])
+        soup = BeautifulSoup(page.text,'html.parser')
+        price = soup.find("p",{"class":"product-sell-price"}).text
+        caracteristiques = soup.findAll("section",{"class":"product-features"})
+        print(price)
+        print(len(caracteristiques))
+
     
     #print(ul)
     #print(lis)
-    print(len(lis))
+    print(len(lis))   
     #print(soup)
 
