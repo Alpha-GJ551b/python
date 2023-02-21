a5e1868b07addaafaf726ae4f8efe1e6687bb970
Kevin Romagne <kevin.69.personnel@gmail.com>
2023-02-21 11:57:45 +0100

    start scraping

--- scrap.py
+++ scrap.py
@@ -1,2 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.lego.com/fr-fr"
+uri = ""
+page = requests.get(url + uri)
+
+# Voir le code html source
+print(page.ok)
+#print(page.content)
+
+if page.ok:
+    soup = BeautifulSoup(page.text,'html.parser')
+    ul = soup.find("ul")
+    uls = soup.findAll("ul",{"class":"results"})
+    lis = ul.findAll("li")
+    for li in lis:
+        a =li.find("a")
+        print(a)
+        print(url + a["href"])
+        
+    print(lis)
+    print(len(uls))
+    print(soup)
+
+
 
 
